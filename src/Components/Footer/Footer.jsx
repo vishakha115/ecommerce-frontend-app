99b5a2dc5f955368352bb201f68e20f804d5b717
Vishakha Shelar <147900817+vishakha115@users.noreply.github.com>
2025-01-08 14:41:31 +0530

    Update Footer.jsx
    small change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
       </div>
       <div className="footer-copyright">
         <hr />
-        <p>Copyright @ 2023 - All Right Reserved.</p>
+        <p>Copyright @ 2025 - All Right Reserved.</p>
       </div>
     </div>
   )
